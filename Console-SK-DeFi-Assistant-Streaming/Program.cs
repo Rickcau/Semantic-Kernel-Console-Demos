988f6c50b4fbb9179b81977e30fa8f9215d7dd19
Rick Caudle <rickcau@hotmail.com>
2024-02-14 13:13:28 -0500

    Add project files.

---
+++ Console-SK-DeFi-Assistant-Streaming/Program.cs
@@ -0,0 +1,86 @@
+﻿using Console_SK_DeFi_Assistant.Plugins;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel;
+using System.Configuration;
+using System.Net;
+using Azure.Core;
+using System;
+
+Console.WriteLine("This example makes use of an SK Plugin that calls an endpoint and use Chat History in a very simple Assistant Scenario");
+Console.WriteLine("Since this example is using Plugin that Queries the Uniswap V3 Subgraph you need to ask questions that make sense to be used in a query.");
+Console.WriteLine("Example 1: top 10 active pools");
+Console.WriteLine("Example 2: Retrieve 10 most liquid pools");
+Console.WriteLine("{ pools(orderBy: volumeUSD, orderDirection: desc, first: 10) { id volumeUSD } }");
+Console.WriteLine("Type: Exit to exit the chat");
+Console.WriteLine("Streaming is used!");
+
+
+var builder = Kernel.CreateBuilder();
+
+var openAiDeployment = ConfigurationManager.AppSettings.Get("AzureOpenAIModel");
+var openAiUri = ConfigurationManager.AppSettings.Get("AzureOpenAIEndpoint");
+var openAiApiKey = ConfigurationManager.AppSettings.Get("AzureOpenAIKey");
+
+if (openAiDeployment != null && openAiUri != null && openAiApiKey != null)
+{
+    builder.Services.AddAzureOpenAIChatCompletion(
+    deploymentName: openAiDeployment,
+    endpoint: openAiUri,
+    apiKey: openAiApiKey);
+}
+builder.Plugins.AddFromType<UniswapV3SubgraphPlugin>();
+
+var kernel = builder.Build();
+
+ChatHistory history = [];
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+while (true)
+{
+    Console.Write(">> ");
+    var userMessage = Console.ReadLine();
+    if (userMessage != "Exit")
+    {
+        // history.AddUserMessage(Console.ReadLine()!);
+        history.AddUserMessage(userMessage!);
+
+        OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+        {
+            ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+        };
+
+        bool roleWritten = false;
+        string fullMessage = string.Empty;
+    
+        try
+        {
+            await foreach (var chatUpdate in chatCompletionService.GetStreamingChatMessageContentsAsync(history,executionSettings: openAIPromptExecutionSettings, kernel: kernel))
+            {
+                if (!roleWritten && chatUpdate.Role.HasValue)
+                {
+                    Console.Write($"{chatUpdate.Role.Value}: {chatUpdate.Content}");
+                    roleWritten = true;
+                }
+
+                if (chatUpdate.Content is { Length: > 0 })
+                {
+                    fullMessage += chatUpdate.Content;
+                    Console.Write(chatUpdate.Content);
+                }
+            }
+
+            Console.WriteLine("\n------------------------");
+            history.AddMessage(AuthorRole.Assistant, fullMessage);
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+    else break;
+}
+
+
---
+++ README.md
@@ -0,0 +1,30 @@
+# Semantic Kernel Console App Example Playground
+
+In this Solution / Repo I have several Console Apps that I demo various SK examples.  I often leverage console apps to learn new concepts and to play with different patterns as it's a quick way to get up an running.  I hope you find this repo useful.
+
+## Console-SK-DeFi-Assistant
+In this example, I create a custom SK plugin for the Uniswap V3 Subgraph. I also make use of ToolCallBehavior.AutoInvokeKernelFunctions setting so the Kernel will automatically determine if there is a Native Function it needs to invoke.  
+
+When you run the example, you can ask general questions like you would with any ChatBot, but if you ask a question that aligns to the description of the SK Function the Kernel will invoke the function passing in a GraphQL query.  What is interesting about this, is the the AI will general the GraphQL query for you, or you can actually provide a GraphQL query.
+
+In this example, I am not streaming the Chat Completion so you have to wait for the AI to finish.  If you are interesting in seeing a Streaming example, take a look at the next example.
+
+## Console-SK-DeFi-Assistant-Streaming
+This is the same example as the above, but I have implementing Streaming logic, which results in a better Chat experience, and would be the recommended approach for a production solution.
+
+## Requirements for this example
+
+1. Rename the App.config.bak to App.config
+2. Modify the App.config with your LLM/AI details.
+
+    ~~~
+			<?xml version="1.0" encoding="utf-8" ?>
+			<configuration>
+				<appSettings>
+					<add key="AzureOpenAIEndpoint" value="<AzureOpenAI Endpoint URI>" />
+					<add key="AzureOpenAIKey" value="AzureOpenAI KEY" />  
+					<add key="AzureOpenAIModel" value="AzureOpenAI Model Name" />
+				</appSettings>
+			</configuration>
+    ~~~
+ 
